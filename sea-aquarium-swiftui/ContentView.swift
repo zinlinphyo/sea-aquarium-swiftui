e5e62ff3be24ec65d49f381df878ec624c743b22
Zin Lin Phyo <zinlinphyo.dev@gmail.com>
2024-11-13 17:07:17 +0700

    Initial Commit

---
+++ sea-aquarium-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sea-aquarium-swiftui
+//
+//  Created by Zin Lin Phyo on 13/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sea-aquarium-swiftui/sea_aquarium_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sea_aquarium_swiftuiApp.swift
+//  sea-aquarium-swiftui
+//
+//  Created by Zin Lin Phyo on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct sea_aquarium_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
